e9b1fb9d7dc577678f4b9c968814be8f33da4910
Ignacio Iglesias <iiglesiascampoy@gmail.com>
2020-11-19 20:28:14 -0700

    Avance en interfaz

--- app/src/main/java/com/cracks/tictactoe/MainActivity.kt
+++ app/src/main/java/com/cracks/tictactoe/MainActivity.kt
@@ -2,10 +2,17 @@ package com.cracks.tictactoe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun buttonClick(view: View) {
+        /*1. Manejar el evento tap */
+        /*2. Evaluar el tablero tras cada jugada */
+
     }
 }
